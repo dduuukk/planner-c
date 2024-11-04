6207f6481573dedd5ec378a12edd7508653dca62
Christian Bender <114115825+dduuukk@users.noreply.github.com>
2024-11-04 10:26:12 -0800

    Updated base structure to start working with CMake #1

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
