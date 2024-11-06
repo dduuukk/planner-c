b1aa940707b50a097df721086f196bcc7ca10e87
dduuukk <bender.c@northeastern.edu>
2024-11-05 16:46:09 -0800

    Updated to C++ linker for CMake #2

---
+++ src/task/task.h
@@ -0,0 +1,21 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+typedef struct {
+    char* name;
+    char* description;
+} Task;
+
+// Task* createTask(const char* name, const char* description);
+// const char* getTaskName(const Task* task);
+// const char* getTaskDescription(const Task* task);
+// void setTaskName(Task* task, const char* name);
+// void setTaskDescription(Task* task, const char* description);
+// void printTask(const Task* task);
+// void freeTask(Task* task);
+
+#endif // TASK_H
